14815de0cbf8bf243b5d53fea884a5e9e418d65e
Xerath <yoondj98@naver.com>
2023-05-30 07:47:46 +0900

    solve 2012
    input 입력 시 좀 더 간결하게 만드는 방법 사용(sys)

--- 98/Greedy/2012.py
+++ 98/Greedy/2012.py
@@ -1,9 +1,12 @@
-n = int(input())
-scores=[]
-for i in range(n):
-    scores.append(int(input()))
+import sys
+
+ip = sys.stdin.readline
+n = int(ip())
+scores = []
+for _ in range(n):
+    scores.append(int(ip()))
 scores.sort()
 count = 0
-for idx in range(1, n+1):
-    count += abs(scores[idx-1]-idx)
+for idx in range(1, n + 1):
+    count += abs(scores[idx - 1] - idx)
 print(count)
