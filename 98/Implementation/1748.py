33b8ec84b3a2ca683c4dd56f88d65c97e21e0c69
Xerath <yoondj98@naver.com>
2023-06-11 09:17:19 +0900

    solve 1748

--- 98/Implementation/1748.py
+++ 98/Implementation/1748.py
@@ -3,5 +3,5 @@ n = sys.stdin.readline().rstrip()
 result = 0
 for i in range(0, len(n)-1):
   result+=9*(10**i)*(i+1)
-result+=(int(n)%(10**(len(n)-1))+1)*len(n)
+result+=(int(n)-10**(len(n)-1)+1)*len(n)
 print(result)
