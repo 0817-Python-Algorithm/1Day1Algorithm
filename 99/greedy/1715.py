137bd295d8d1c9ccdcf0c97151051f9434e73928
parksk99 <qkrtjdrb99@naver.com>
2023-08-23 00:19:00 +0900

    solve 1715

---
+++ 99/greedy/1715.py
@@ -0,0 +1,14 @@
+import sys
+import heapq
+input = sys.stdin.readline
+N = int(input())
+cards = []
+for _ in range(N):
+    heapq.heappush(cards, int(input()))
+answer = 0
+while(len(cards) > 1):
+    a = heapq.heappop(cards)
+    b = heapq.heappop(cards)
+    answer += a+b
+    heapq.heappush(cards, a+b)
+print(answer)
