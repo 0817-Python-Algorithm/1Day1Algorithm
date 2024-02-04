721ea7fff727d125d3376afddd42580f01463185
Xerath <yoondj98@naver.com>
2024-02-04 23:24:36 +0900

    solving Programmers_42892_32

---
+++ 98/ProgrammersAlgorithm/Programmers_42892_32.py
@@ -0,0 +1,14 @@
+class Node:
+    def __init__(self, key):
+        self.number = key
+        self.left = []
+        self.right = []
+        
+def solution(nodeinfo):
+    
+    
+
+    preorder = []
+    postorder = []
+    answer = [[]]
+    return answer
