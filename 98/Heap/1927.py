753b67734b0dde4f6ef74b0b00a9810247c5b7e1
Xerath <yoondj98@naver.com>
2024-05-20 21:05:15 +0900

    solve BOJ 1927

---
+++ 98/Heap/1927.py
@@ -0,0 +1,17 @@
+import sys
+import heapq
+
+input = sys.stdin.readline
+
+N = int(input())
+
+heap = []
+for i in range(N):
+    num = int(input())
+    if num == 0:
+        if len(heap) == 0:
+            print(0)
+        else:
+            print(heapq.heappop(heap))
+    else:
+        heapq.heappush(heap, num)
