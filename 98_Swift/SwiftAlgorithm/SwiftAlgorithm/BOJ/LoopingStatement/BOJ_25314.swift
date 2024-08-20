b995da10cce3431e7c57721b031e5e5d2dd577e8
윤동주 / Xerath <54929503+yoondj98@users.noreply.github.com>
2024-08-21 01:08:47 +0900

    solve BOJ 25314

---
+++ 98_Swift/SwiftAlgorithm/SwiftAlgorithm/BOJ/LoopingStatement/BOJ_25314.swift
@@ -0,0 +1,12 @@
+//
+//  BOJ_25314.swift
+//  SwiftAlgorithm
+//
+//  Created by 윤동주 on 8/21/24.
+//
+
+import Foundation
+
+let N = Int(readLine()!)!
+
+print(Array(repeating: "long", count: N/4).joined(separator: " ") + " int")
